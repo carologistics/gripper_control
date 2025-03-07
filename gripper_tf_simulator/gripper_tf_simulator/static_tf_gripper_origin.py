4a926d4cc77c4fd8b7f8ec3380be3daa3cd8fbdd
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2025-03-07 14:45:11 +0100

    project: fix formatting and style according to pre-commit

--- gripper_tf_simulator/gripper_tf_simulator/static_tf_gripper_origin.py
+++ gripper_tf_simulator/gripper_tf_simulator/static_tf_gripper_origin.py
@@ -1,77 +1,88 @@
-from geometry_msgs.msg import TransformStamped
-
+# Copyright (c) 2025 Carologistics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import rclpy
+from geometry_msgs.msg import TransformStamped
 from rclpy.node import Node
-
 from tf2_ros import TransformBroadcaster
 
 
 class FixedFrameBroadcaster(Node):
 
-   def __init__(self):
-       super().__init__('fixed_frame_tf2_broadcaster')
-       self.tf_broadcaster = TransformBroadcaster(self)
-       self.timer = self.create_timer(0.1, self.broadcast_timer_callback)
+    def __init__(self):
+        super().__init__("fixed_frame_tf2_broadcaster")
+        self.tf_broadcaster = TransformBroadcaster(self)
+        self.timer = self.create_timer(0.1, self.broadcast_timer_callback)
 
-   def broadcast_timer_callback(self):
-       t = TransformStamped()
+    def broadcast_timer_callback(self):
+        t = TransformStamped()
 
-       t.header.stamp = self.get_clock().now().to_msg()
-       t.header.frame_id = 'gripper_y_dyn'
-       t.child_frame_id = 'gripper_x_origin'
-       t.transform.translation.x = 0.8125
-       t.transform.translation.y = 0.54
-       t.transform.translation.z = -0.18
-       t.transform.rotation.x = 0.0
-       t.transform.rotation.y = 0.0
-       t.transform.rotation.z = 0.0
-       t.transform.rotation.w = 1.0
+        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.frame_id = "gripper_y_dyn"
+        t.child_frame_id = "gripper_x_origin"
+        t.transform.translation.x = 0.8125
+        t.transform.translation.y = 0.54
+        t.transform.translation.z = -0.18
+        t.transform.rotation.x = 0.0
+        t.transform.rotation.y = 0.0
+        t.transform.rotation.z = 0.0
+        t.transform.rotation.w = 1.0
 
-       self.tf_broadcaster.sendTransform(t)
+        self.tf_broadcaster.sendTransform(t)
 
-       t2 = TransformStamped()
+        t2 = TransformStamped()
 
-       t2.header.stamp = self.get_clock().now().to_msg()
-       t2.header.frame_id = 'gripper_z_dyn'
-       t2.child_frame_id = 'gripper_y_origin'
-       t2.transform.translation.x = -0.1325
-       t2.transform.translation.y = 0.0
-       t2.transform.translation.z = 0.65
-       t2.transform.rotation.x = 0.0
-       t2.transform.rotation.y = 0.0
-       t2.transform.rotation.z = 0.0
-       t2.transform.rotation.w = 1.0
-       self.tf_broadcaster.sendTransform(t2)
+        t2.header.stamp = self.get_clock().now().to_msg()
+        t2.header.frame_id = "gripper_z_dyn"
+        t2.child_frame_id = "gripper_y_origin"
+        t2.transform.translation.x = -0.1325
+        t2.transform.translation.y = 0.0
+        t2.transform.translation.z = 0.65
+        t2.transform.rotation.x = 0.0
+        t2.transform.rotation.y = 0.0
+        t2.transform.rotation.z = 0.0
+        t2.transform.rotation.w = 1.0
+        self.tf_broadcaster.sendTransform(t2)
 
-       t3 = TransformStamped()
+        t3 = TransformStamped()
 
-       t3.header.stamp = self.get_clock().now().to_msg()
-       t3.header.frame_id = 'base_link'
-       t3.child_frame_id = 'gripper_z_origin'
-       t3.transform.translation.x = 0.245
-       t3.transform.translation.y = 0.0
-       t3.transform.translation.z = 2.5
-       t3.transform.rotation.x = 0.0
-       t3.transform.rotation.y = 0.0
-       t3.transform.rotation.z = 0.0
-       t3.transform.rotation.w = 1.0
+        t3.header.stamp = self.get_clock().now().to_msg()
+        t3.header.frame_id = "base_link"
+        t3.child_frame_id = "gripper_z_origin"
+        t3.transform.translation.x = 0.245
+        t3.transform.translation.y = 0.0
+        t3.transform.translation.z = 2.5
+        t3.transform.rotation.x = 0.0
+        t3.transform.rotation.y = 0.0
+        t3.transform.rotation.z = 0.0
+        t3.transform.rotation.w = 1.0
 
-       self.tf_broadcaster.sendTransform(t3)
+        self.tf_broadcaster.sendTransform(t3)
 
-       t4 = TransformStamped()
+        t4 = TransformStamped()
 
-       t4.header.stamp = self.get_clock().now().to_msg()
-       t4.header.frame_id = 'gripper_x_dyn'
-       t4.child_frame_id = 'gripper_final'
-       t4.transform.translation.x = 0.46528
-       t4.transform.translation.y = 0.0
-       t4.transform.translation.z = -0.405
-       t4.transform.rotation.x = 0.0
-       t4.transform.rotation.y = 0.0
-       t4.transform.rotation.z = 0.0
-       t4.transform.rotation.w = 1.0
+        t4.header.stamp = self.get_clock().now().to_msg()
+        t4.header.frame_id = "gripper_x_dyn"
+        t4.child_frame_id = "gripper_final"
+        t4.transform.translation.x = 0.46528
+        t4.transform.translation.y = 0.0
+        t4.transform.translation.z = -0.405
+        t4.transform.rotation.x = 0.0
+        t4.transform.rotation.y = 0.0
+        t4.transform.rotation.z = 0.0
+        t4.transform.rotation.w = 1.0
 
-       self.tf_broadcaster.sendTransform(t4)
+        self.tf_broadcaster.sendTransform(t4)
 
     #    t5 = TransformStamped()
 
