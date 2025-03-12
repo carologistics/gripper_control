b3691d3799f13e2cdb64a6bf66ff756daee41ffb
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2025-03-12 21:50:31 +0100

    gigatino: better close-distance handling of s controller
    Correct overshoot and use motor-specific thresholds.

--- gigatino/lib/robotino_hat/s_controller/s_controller.h
+++ gigatino/lib/robotino_hat/s_controller/s_controller.h
@@ -1,16 +1,16 @@
 class SCurveMotionController {
 public:
   SCurveMotionController(float min_speed, float max_speed, float max_accel,
-                         float max_jerk, float short_dist)
+                         float max_jerk, float short_dist, float threshold)
       : min_speed_(min_speed), max_speed_(max_speed), max_accel_(max_accel),
-        max_jerk_(max_jerk), short_dist_(short_dist), speed_(0), accel_(0),
-        peak_speed_(0) {}
+        max_jerk_(max_jerk), short_dist_(short_dist), threshold_(threshold),
+        speed_(0), accel_(0), peak_speed_(0) {}
 
   float compute(float target, float current, float dt);
   void reset();
   void plan_curve(float start_pos, float target_pos);
 
-  float min_speed_, max_speed_, max_accel_, max_jerk_, short_dist_;
+  float min_speed_, max_speed_, max_accel_, max_jerk_, short_dist_, threshold_;
   float speed_, accel_, peak_speed_, peak_accel_;
 
   bool need_accel;
