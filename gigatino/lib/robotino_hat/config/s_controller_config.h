11f0b894e3d7631aa422d05a5dcb02d4e9b3ae55
Tarik Viehmann <viehmann@kbsg.rwth-aachen.de>
2025-03-28 16:52:35 +0100

    gigatino: adapt speed of motors

--- gigatino/lib/robotino_hat/config/s_controller_config.h
+++ gigatino/lib/robotino_hat/config/s_controller_config.h
@@ -4,13 +4,21 @@
 #include <config/stepper_config.h>
 
 // in units (per second ^0 ^1 ^2 or ^3, respectively)
-#define MOT_X_MOTION_MIN_SPEED 10.f
-#define MOT_X_MOTION_MAX_SPEED 800.f
-#define MOT_X_MOTION_MAX_ACCEL 3200.f
-#define MOT_X_MOTION_MAX_JERK 4000.f
-#define MOT_X_MOTION_SHORT_DIST 10.f // mm
 
-#define MOT_YAW_MOTION_MIN_SPEED 50.f
+#define MOT_X_MOTION_MIN_SPEED 30.f
+#define MOT_X_MOTION_MAX_SPEED 800.f
+#define MOT_X_MOTION_MAX_ACCEL 2000.f
+#define MOT_X_MOTION_MAX_JERK 2000.f
+#define MOT_X_MOTION_SHORT_DIST 10.f // mm
+/**
+#define MOT_X_MOTION_MIN_SPEED 100.f
+#define MOT_X_MOTION_MAX_SPEED 100.f
+#define MOT_X_MOTION_MAX_ACCEL 200.f
+#define MOT_X_MOTION_MAX_JERK 400.f
+#define MOT_X_MOTION_SHORT_DIST 10000.f // mm
+ */
+
+#define MOT_YAW_MOTION_MIN_SPEED 20.f
 #define MOT_YAW_MOTION_MAX_SPEED 800.f
 #define MOT_YAW_MOTION_MAX_ACCEL 3200.f
 #define MOT_YAW_MOTION_MAX_JERK 4000.f
