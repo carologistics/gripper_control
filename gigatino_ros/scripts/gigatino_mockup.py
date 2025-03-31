4b17abed1849ea265689d3dbbe27b1b49194150d
Zhen Yan Khaw <zhen-yan.khaw@alumni.fh-aachen.de>
2025-03-31 16:08:16 +0200

    Gripper : remove print

--- gigatino_ros/scripts/gigatino_mockup.py
+++ gigatino_ros/scripts/gigatino_mockup.py
@@ -105,7 +105,6 @@ def listen_for_messages():
             continue
         with variable_lock:
             curr_command = unpacked_data
-            print(unpacked_data, flush=True)
             if "command_index" in unpacked_data:
                 curr_feedback["command_index"] = unpacked_data["command_index"]
         # Cancel the previous delay (if any) before starting a new one
